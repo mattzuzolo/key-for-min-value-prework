e1d3b1ac75d1f3549e11c9e2c1b40eb6dd894f99
Matt Zuzolo <mattzuzolo@outlook.com>
2018-04-27 19:20:23 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,59 @@
 
 def key_for_min_value(name_hash)
 
-end
+    smallest_key = nil
+    smallest_value = 0
+
+    if name_hash.size == 0
+        nil
+    
+    else
+        name_hash.each do |key,value|
+            if value < smallest_value || smallest_value == 0
+              smallest_value = value
+              smallest_key = key
+            end #end if value < smallest_value
+          
+        end  #end .each
+    
+         
+    end #end if/else
+    
+    
+     smallest_key
+end #end big method
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# else
+#         name_hash.each do |key,value|
+#           key_array << key
+#           value_array << value
+#         end  
+        
+#         while i < value_array.length
+          
+#           if value_array[i] > smallest
+#             smallest_key = key_array[i]
+#           end  
+          
+#           i += 1
+      
+#         end  #end while  
+        
+#         smallest_key
+
+
+
+
